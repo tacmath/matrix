76807dbfa40eb980b587b4441d11cdb0d56cef47
tac <tacmathys@gmail.com>
2022-10-25 22:58:36 +0200

    a lil more tests

--- ex11/main.cpp
+++ ex11/main.cpp
@@ -19,6 +19,13 @@ int main (void) {
     std::cout << "determinant : " << m.determinant() << std::endl << "------------------" << std::endl;
 
     m = Matrix<>({{8., 5., -2., 4.}, {4., 2.5, 20., 4.}, {8., 5., 1., 4.}, {28., -4., 17., 1.}});
+    std::cout << "determinant : " << m.determinant() << std::endl << "------------------" << std::endl;
+
+    m = Matrix<>(0, 0);
+    std::cout << "determinant : " << m.determinant() << std::endl << "------------------" << std::endl;;
+
+    m = Matrix<>(1, 1);
+    m[0][0] = 10;
     std::cout << "determinant : " << m.determinant() << std::endl;
     return (0);
 }
